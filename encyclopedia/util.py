f0d800d6331a267bf97d3fae26ed762959b0ae8b
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 15:35:34 +0600

    Add convert_to_html helper function
    It uses markdown2 module to convert markdown content into html.

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -1,4 +1,5 @@
 import re
+from . import markdown2
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
@@ -35,3 +36,12 @@ def get_entry(title):
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
+
+
+# Some custom functions
+
+def convert_to_html(md_content):
+    """
+    Converts the markdown-formatted text to html using markdown2 module.
+    """
+    return markdown2.markdown(md_content)
