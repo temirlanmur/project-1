3b323a4ce228d84e18ef31565da0fc3218158082
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 15:39:10 +0600

    Add URL pattern for displaying "not found" page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_detail, name="entry_detail")
+    path("wiki/<str:title>", views.entry_detail, name="entry_detail"),
+    path("not-found", views.not_found, name="not_found")
 ]
