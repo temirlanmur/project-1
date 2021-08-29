cd8b930fd721b0f59a9f3dff97b6355c8dd283d3
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 16:14:13 +0600

    Add URL patter for "Random Entry" page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry_detail, name="entry_detail"),
     path("create-new-entry", views.create_new_entry, name="create_new_entry"),
     path("search", views.search, name="search"),
+    path("random-entry", views.random_entry, name="random_entry"),
     path("not-found", views.not_found, name="not_found")
 ]
