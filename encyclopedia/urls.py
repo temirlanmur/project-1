3263cc93b39e69cf93e5e44d407da4faec731023
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 16:09:11 +0600

    Add URL pattern for search results page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/<str:title>/edit", views.edit_entry, name="edit_entry"),
     path("wiki/<str:title>", views.entry_detail, name="entry_detail"),
     path("create-new-entry", views.create_new_entry, name="create_new_entry"),
+    path("search", views.search, name="search"),
     path("not-found", views.not_found, name="not_found")
 ]
