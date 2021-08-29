c394e930149e9a54ec2f188bcd5e60c2d6b02547
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 15:49:28 +0600

    Add edit_entry function view
    This view firstly checks the existence of the entry to edit. If
    the request is for non-existent entry, it redirects user to not found
    page.
    Then, this view proceeds according to the request method. For POST
    it binds the form with POST data, validates it, and if validation
    successful, saves the entry and redirects the user to its page.
    If validation was not successful, the view returns populated form
    back to the user.
    For GET (& other methods) it binds the form with existing data, and
    passes the form to context.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -20,5 +20,28 @@ def entry_detail(request, title):
     return redirect('not_found')
 
 
+def edit_entry(request, title):
+    if not util.get_entry(title):
+        return redirect('not_found')
+
+    if request.method == "POST":
+        form = util.EntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]             
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            return redirect('entry_detail', title)         
+    else:
+        data = {'title': title,
+                'content': util.get_entry(title)}
+        form = util.EntryForm(initial=data)
+        form.hide_title()
+    
+    return render(request, 'encyclopedia/edit_entry.html', {
+        'title': title,
+        'form': form
+    })
+
+
 def not_found(request):
     return render(request, 'encyclopedia/not_found.html')
