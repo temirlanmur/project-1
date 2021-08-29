cff914bb5e59a0a5663521ee290e49ecd67d41a9
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 15:44:19 +0600

    Add URL pattern for the "Edit Entry" page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/<str:title>/edit", views.edit_entry, name="edit_entry"),
     path("wiki/<str:title>", views.entry_detail, name="entry_detail"),
     path("not-found", views.not_found, name="not_found")
 ]
