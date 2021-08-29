4bd12a5760bd82cf5bf396a337b862ef27f77430
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-29 15:59:40 +0600

    Add pattern for page where one can create entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/edit", views.edit_entry, name="edit_entry"),
     path("wiki/<str:title>", views.entry_detail, name="entry_detail"),
+    path("create-new-entry", views.create_new_entry, name="create_new_entry"),
     path("not-found", views.not_found, name="not_found")
 ]
